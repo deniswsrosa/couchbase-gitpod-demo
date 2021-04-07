9f5b4a2742671291fe9c0372b64cb380c4a167fa
Denis Rosa <denisrosa@MacBook-Pro.local>
2021-04-07 14:19:37 +0200

    test

--- scripts/start-cb.sh
+++ scripts/start-cb.sh
@@ -5,4 +5,3 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8091)" != "301" ]
 cd /opt/couchbase/bin
 ./couchbase-cli cluster-init -c localhost --cluster-username Administrator --cluster-password password --services data,index,query --cluster-ramsize 512
 ./couchbase-cli bucket-create -c localhost:8091 --username Administrator --password password --bucket default --bucket-type couchbase --bucket-ramsize 256
-/bin/bash
