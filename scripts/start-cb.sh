dd99455095e4d83655afff3ad0926adacb479038
Denis Rosa <denisrosa@MacBook-Pro.local>
2021-04-08 12:14:20 +0200

    test

--- scripts/start-cb.sh
+++ scripts/start-cb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./etc/service/couchbase-server/run &
+exec /opt/couchbase/bin/couchbase-server -- -kernel global_enable_tracing false -noinput &
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8091)" != "301" ]]; do sleep 5; done
 cd /opt/couchbase/bin
 ./couchbase-cli cluster-init -c localhost --cluster-username Administrator --cluster-password password --services data,index,query --cluster-ramsize 512
